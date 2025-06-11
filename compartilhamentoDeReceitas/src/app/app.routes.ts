71b2dee2105015f0cc9664646515cd4be49826ef
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-06-11 20:16:33 -0300

    commit

--- compartilhamentoDeReceitas/src/app/app.routes.ts
+++ compartilhamentoDeReceitas/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ListagemComponent } from './pages/listagem/listagem.component';
+import { CadastroComponent } from './pages/components/cadastro/cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      redirectTo: 'cadastroReceita',
+      pathMatch: 'full'
+    },
+    {
+      path: 'listagem',
+      component: ListagemComponent
+    },
+    {
+      path: 'cadastroReceita',
+      component: CadastroComponent
+    }
+];
